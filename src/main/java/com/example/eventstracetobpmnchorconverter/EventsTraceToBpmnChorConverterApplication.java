8503ee86d3c1975d5cded613b4bd27baef48f0bc
Eugen Gerb <egb05@gmx.de>
2023-02-07 17:12:32 +0100

    Initial commit

---
+++ src/main/java/com/example/eventstracetobpmnchorconverter/EventsTraceToBpmnChorConverterApplication.java
@@ -0,0 +1,13 @@
+package com.example.eventstracetobpmnchorconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsTraceToBpmnChorConverterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventsTraceToBpmnChorConverterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/eventstracetobpmnchorconverter/EventsTraceToBpmnChorConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eventstracetobpmnchorconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsTraceToBpmnChorConverterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
